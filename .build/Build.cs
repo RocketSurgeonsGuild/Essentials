622f0a65b9edbab4f1eaa94cf1569e2f73d97bda
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-07-18 01:02:34 -0500

    (build) Added Nuke Build
    - Modified azure-pipelines.yml to use Nuke
    - Added NetFramework Nuke Build
    - Added nuke-job.yml

---
+++ .build/Build.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Rocket.Surgery.Nuke.MsBuild;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Program : MSBuild
+{
+    /// <summary>
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+    /// </summary>
+
+    public static int Main() => Execute<Program>(x => x.Default);
+
+    Target Default => _ => _.DependsOn(NetFramework);
+}
